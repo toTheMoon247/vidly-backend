43efc72e83d449cc5f848bee240a54f68dd323d1
tothemoon247 <samuel1haim@gmail.com>
2021-02-27 20:57:52 +0000

    Implement genre API
    Add routers for CRUD operations for genres API

--- index.js
+++ index.js
@@ -1,5 +1,75 @@
+const Joi = require('joi'); // Joi is a data validation package
 const express = require('express');
 const app = express();
+
+app.use(express.json()); // This is a build-in middleware in Express. Output: req.body (JSON object)
+
+// For now we save it locally, instead of the DB
+const genres = [
+	{ id: 1, name: 'action' },
+	{ id: 2, name: 'horror' },
+	{ id: 2, name: 'drama' },
+];
+
+// Create
+app.post('/api/genres', (req, res) => {
+	// Check for errors - if there are errors return to the client
+	const { error } = validateGenre(req.body);
+	if (error)
+		return res.status(400).send(error.details[0].message);
+
+	// Create a genre object
+	const genre = {
+		id: genres.length + 1,
+		name: req.body.name
+	};
+
+	// Update the "Database"
+	genres.push(genre);
+	res.send(genre);
+
+	// Update the client
+	res.send(genre);
+});
+
+// Read
+app.get('/api/genres', (req, res) => {
+	res.send(genres);
+});
+
+app.get('/api/genres/:id', (req, res) => {
+	// Find the genre
+	const genre = genres.find(c => c.id === parseInt(req.params.id));
+	if (!genre)
+		return res.status(404).send("couldn't find genre id");
+
+	res.send(genre);
+});
+
+// Update
+app.put('/api/genres/:id', (req, res) => {
+  const genre = genres.find(c => c.id === parseInt(req.params.id));
+  if (!genre) 
+  	return res.status(404).send('The genre with the given ID was not found.');
+
+  const { error } = validateGenre(req.body); 
+  if (error) 
+  	return res.status(400).send(error.details[0].message);
+  
+  genre.name = req.body.name; 
+  res.send(genre);
+});
+
+// Delete
+app.delete('/api/genres/:id', (req, res) => {
+  const genre = genres.find(c => c.id === parseInt(req.params.id));
+  if (!genre) return res.status(404).send('The genre with the given ID was not found.');
+
+  const index = genres.indexOf(genre);
+  genres.splice(index, 1);
+
+  res.send(genre);
+});
 
 app.get('/', (req, res) => {
 	res.send('Hello World');
@@ -9,4 +79,5 @@ app.get('/api/courses', (req, res) => {
 	res.send([1, 2, 3, 4]);
 });
 
-app.listen(3000, () => console.log('Listening on port 3000')); 
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`)); 
