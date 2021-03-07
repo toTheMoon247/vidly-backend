aa9cd6677e90968c6c9019abcae657a230bc3af8
tothemoon247 <samuel1haim@gmail.com>
2021-03-07 17:26:08 +0000

    movies api: movies router - wip

--- routes/movies.js
+++ routes/movies.js
@@ -1,1 +1,5 @@
-const { Movie, validate }
+const { Movie, validate } = require("../models/movie");
+const { genre } = require("../models/genre");
+const mogoose = require("mongoose");
+const express = require("express");
+const router = express.Router();
